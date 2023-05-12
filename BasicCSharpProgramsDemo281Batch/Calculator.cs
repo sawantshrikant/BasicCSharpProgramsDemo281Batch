ff62aa75a83b60d03a8ae73f0dbc5aefd775ffe9
Shrikant <sawantshrikant07@gmail.com>
2023-05-12 11:06:09 +0530

    [Shrikant] add:CalculatorFor2NoOperation

---
+++ BasicCSharpProgramsDemo281Batch/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BasicCSharpProgramsDemo281Batch_
+{
+    internal class Calculator
+    {
+        public void AddTwoNumbers(int x, int y)
+        {
+            int result = x + y;
+            Console.WriteLine($"Addition of {x} and {y} is: {result}");
+        }
+
+        public void SubtractTwoNumbers(int x, int y)
+        {
+            int result = x - y;
+            Console.WriteLine($"Subtraction of {x} and {y} is: {result}");
+        }
+
+        public void MultiplyTwoNumbers(int x, int y)
+        {
+            int result = x * y;
+            Console.WriteLine($"Multiplication of {x} and {y} is: {result}");
+        }
+
+        public void DivideTwoNumbers(int x, int y)
+        {
+            if (y != 0)
+            {
+                double result = (double)x / y;
+                Console.WriteLine($"Division of {x} and {y} is: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Cannot divide by zero.");
+            }
+        }
+    }
+}
--- BasicCSharpProgramsDemo281Batch/Program.cs
+++ BasicCSharpProgramsDemo281Batch/Program.cs
@@ -1,7 +1,31 @@
-﻿internal class Program
+﻿using BasicCSharpProgramsDemo281Batch_;
+using System;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
         Console.WriteLine("Welcome To Basic CSharp Programs Demo 281Batch!");
+        Console.WriteLine("Please choose any one Program from below option");
+        Console.WriteLine("1. Calculator\n2. Fibonnoci Series");
+        int option = Convert.ToInt32(Console.ReadLine());
+        switch (option)
+        {
+            case 1:
+                Console.WriteLine("Welcome to Basic C# Programs");
+                Calculator calculator = new Calculator();
+                Console.WriteLine("Please enter the first number");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the second number");
+                int y = Convert.ToInt32(Console.ReadLine());
+                calculator.AddTwoNumbers(x, y);
+                calculator.SubtractTwoNumbers(x, y);
+                calculator.MultiplyTwoNumbers(x, y);
+                calculator.DivideTwoNumbers(x, y);
+                break;
+            default:
+                Console.WriteLine("Please choose an existing program");
+                break;
+        }
     }
 }
